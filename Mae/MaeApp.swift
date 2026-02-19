306186e5920851ca28b985b7b6424af10e26b50f
Simi <joao02simi@gmail.com>
2026-02-19 15:28:56 -0300

    Initial Commit

---
+++ Mae/MaeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaeApp.swift
+//  Mae
+//
+//  Created by Joao Simi on 19/02/26.
+//
+
+import SwiftUI
+
+@main
+struct MaeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
